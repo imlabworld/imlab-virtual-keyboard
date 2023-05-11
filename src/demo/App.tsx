9f473a617c19880a2e71fc9a7fdab2f7d814ef1f
HyoungMin <hyoungqu23@gmail.com>
2023-05-12 08:28:41 +0900

    Fix: modify Demo Codes

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,72 +1,16 @@
-import * as React from "react";
-import SimpleKeyboard from "simple-keyboard";
-import Keyboard from "../lib";
-import "./css/App.css";
+import * as React from 'react';
+import './css/App.css';
+import KeyboardHangul from '../lib/components/KeyboardHangul';
 
-class App extends React.Component {
-  state = {
-    input: "",
-    layoutName: "default",
-  };
+const App = () => {
+  const [input, setInput] = React.useState('안녕하세요');
 
-  keyboard: SimpleKeyboard;
-
-  onChange = (input) =>
-    this.setState({ input }, () => console.log("Input changed", input));
-
-  onKeyPress = (button) => {
-    console.log("Button pressed", button);
-
-    /**
-     * Shift functionality
-     */
-    if (["{capslock}", "{shiftleft}", "{shiftright}"].includes(button))
-      this.handleShiftButton();
-  };
-
-  handleShiftButton = () => {
-    const {
-      state: { layoutName },
-    } = this;
-    const shiftToggle = layoutName === "default" ? "shift" : "default";
-
-    this.setState({ layoutName: shiftToggle });
-  };
-
-  onChangeInput = (event) => {
-    const input = event.target.value;
-
-    this.setState({ input: event.target.value }, () =>
-      this.keyboard.setInput(input)
-    );
-  };
-
-  render() {
-    const {
-      state: { input, layoutName },
-      onChangeInput,
-      onChange,
-      onKeyPress,
-    } = this;
-
-    return (
-      <div className="demoPage">
-        <div className="screenContainer">
-          <textarea
-            className="inputContainer"
-            value={input}
-            onChange={onChangeInput}
-          />
-        </div>
-        <Keyboard
-          keyboardRef={(r) => (this.keyboard = r)}
-          onChange={onChange}
-          onKeyPress={onKeyPress}
-          layoutName={layoutName}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <>
+      <p>{input}</p>
+      <KeyboardHangul defaultText={input} getText={setInput} />
+    </>
+  );
+};
 
 export default App;
--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import App from './App';
+import KeyboardHangul from './KeyboardHangul';
 
-const root = document.createElement("div");
-root.className = "root";
+const root = document.createElement('div');
+root.className = 'root';
 
 document.body.appendChild(root);
 
